b9c82e32a2fc9dc487619e38ea49039b082b0cbc
Caioww <sousinhacaio@gmail.com>
2020-07-07 21:42:20 -0300

    Iniciando projeto

---
+++ src/test/java/com/spring/blogsaws/BlogsAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blogsaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
